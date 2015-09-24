4e758179afbc47f3128633fca93a6e5cfa13838b
aadfco <aadf82@gmail.com>
2015-09-24 12:24:57 -0400

    Following along with vid

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+  youtubeVidScroll();
+});
+
+
+function youtubeVidScroll(){
+  var wScroll = $(window).scrollTop();
+
+  $('video.-strip').css('background-position','center  -'+wScroll +'px');
+}
